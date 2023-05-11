e379ec0d2ada1d064e5d3dfd715dadcdab448f84
moforemmanuel <moforemmanuel1000@gmail.com>
2023-05-11 17:12:23 +0100

    added configs

--- next.config.js
+++ next.config.js
@@ -2,9 +2,64 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = withBundleAnalyzer({
-  reactStrictMode: false,
+const defaultRuntimeCaching = require('./cache');
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  // disable: false,
+  disable: process.env.NODE_ENV == 'development',
+  register: true,
+  // skipWaiting: true,
+  // sw: 'ws.js',
+  // scope: '/',
+
+  // cache data for jsonplaceholder
+  // runtimeCaching: defaultRuntimeCaching.concat({
+  //   urlPattern: /^https:\/\/jsonplaceholder\.typicode\.com/,
+  //   handler: 'StaleWhileRevalidate',
+  //   options: {
+  //     cacheName: 'api-cache',
+  //     // cacheableResponse: {
+  //     //   statuses: [200],
+  //     // },
+  //   },
+  // }),
+});
+
+const withPlugins = require('next-compose-plugins');
+
+const nextConfig = {
+  reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
-});
+};
+
+// module.exports = withBundleAnalyzer(withPWA(nextConfig));
+// module.exports = (phase, { defaultConfig }) => {
+//   console.log('phase: ', phase);
+//   console.log('defaultConfig: ', defaultConfig.pwa);
+//   return withPlugins([
+//     [withBundleAnalyzer],
+//     [
+//       withPWA,
+//       {
+//         // other pwa config
+//         // not applicable here, see npmjs
+//       },
+//     ],
+//     nextConfig,
+//   ]);
+// };
+
+module.exports = withPlugins([
+  [withBundleAnalyzer],
+  [
+    withPWA,
+    {
+      // other pwa config
+      // not applicable here, see npmjs
+    },
+  ],
+  nextConfig,
+]);
