60426e7ea207455b0efd59d38bf0d0351b35c266
moforemmanuel <moforemmanuel1000@gmail.com>
2023-05-11 17:14:24 +0100

    modified sw handlers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,26 @@
-import { useState } from 'react';
+/* eslint-disable @typescript-eslint/brace-style */
+/* eslint-disable react/no-invalid-html-attribute */
+// @ts-nocheck
+import { useState, useEffect } from 'react';
 import NextApp, { AppProps, AppContext } from 'next/app';
 import { getCookie, setCookie } from 'cookies-next';
 import Head from 'next/head';
 import { MantineProvider, ColorScheme, ColorSchemeProvider } from '@mantine/core';
 import { Notifications } from '@mantine/notifications';
+// import { Workbox } from 'workbox-window';
+
+// declare global {
+//   interface Window {
+//     workbox: typeof import('workbox-window');
+//   }
+// }
+
+// interface CustomWindow extends Window {
+//   // GA_INITIALIZED?: boolean;
+//   workbox: typeof import('workbox-window');
+// }
+
+// declare const window: CustomWindow;
 
 export default function App(props: AppProps & { colorScheme: ColorScheme }) {
   const { Component, pageProps } = props;
@@ -15,12 +32,186 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
     setCookie('mantine-color-scheme', nextColorScheme, { maxAge: 60 * 60 * 24 * 30 });
   };
 
+  // This hook only run once in browser after the component is rendered for the first time.
+  // It has same effect as the old componentDidMount lifecycle callback.
+  useEffect(() => {
+    if (
+      typeof window !== 'undefined' &&
+      'serviceWorker' in navigator &&
+      window.workbox !== undefined
+    ) {
+      const wb = window.workbox;
+      // const wb = new Workbox('/sw.js');
+      // add event listeners to handle any of PWA lifecycle event
+      // https://developers.google.com/web/tools/workbox/reference-docs/latest/module-workbox-window.Workbox#events
+      wb.addEventListener('installed', (event: Event) => {
+        console.log(`Event ${event.type} is triggered.`);
+        console.log(event);
+      });
+
+      wb.addEventListener('controlling', (event: Event) => {
+        console.log(`Event ${event.type} is triggered.`);
+        console.log(event);
+      });
+
+      wb.addEventListener('activated', (event: Event) => {
+        console.log(`Event ${event.type} is triggered.`);
+        console.log(event);
+      });
+
+      // A common UX pattern for progressive web apps is to show a banner when a service worker has updated and waiting to install.
+      // NOTE: MUST set skipWaiting to false in next.config.js pwa object
+      // https://developers.google.com/web/tools/workbox/guides/advanced-recipes#offer_a_page_reload_for_users
+      const promptNewVersionAvailable = () =>
+        // event: Event
+        {
+          // `event.wasWaitingBeforeRegister` will be false if this is the first time the updated service worker is waiting.
+          // When `event.wasWaitingBeforeRegister` is true, a previously updated service worker is still waiting.
+          // You may want to customize the UI prompt accordingly.
+          if (confirm('A newer version of this web app is available, reload to update?')) {
+            wb.addEventListener('controlling', () =>
+              // event: Event
+              {
+                window.location.reload();
+              }
+            );
+
+            // Send a message to the waiting service worker, instructing it to activate.
+            wb.messageSkipWaiting();
+          } else {
+            console.log(
+              'User rejected to reload the web app, keep using old version. New version will be automatically load when user open the app next time.'
+            );
+          }
+        };
+
+      wb.addEventListener('waiting', promptNewVersionAvailable);
+
+      // ISSUE - this is not working as expected, why?
+      // I could only make message event listenser work when I manually add this listenser into sw.js file
+      wb.addEventListener('message', (event: Event) => {
+        console.log(`Event ${event.type} is triggered.`);
+        console.log(event);
+      });
+
+      /*
+      wb.addEventListener('redundant', event => {
+        console.log(`Event ${event.type} is triggered.`)
+        console.log(event)
+      })
+
+      wb.addEventListener('externalinstalled', event => {
+        console.log(`Event ${event.type} is triggered.`)
+        console.log(event)
+      })
+
+      wb.addEventListener('externalactivated', event => {
+        console.log(`Event ${event.type} is triggered.`)
+        console.log(event)
+      })
+      */
+
+      // never forget to call register as auto register is turned off in next.config.js
+      wb.register();
+    }
+
+    window.addEventListener('offline', () => alert('offline'));
+    window.addEventListener('online', () => alert('online'));
+
+    return () => {
+      window.removeEventListener('offline', () => alert('offline'));
+      window.removeEventListener('online', () => alert('online'));
+    };
+  }, []);
+
   return (
     <>
       <Head>
         <title>Mantine next example</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        {/* <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" /> */}
         <link rel="shortcut icon" href="/favicon.svg" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
+        <meta name="application-name" content="PWA App" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="PWA App" />
+        <meta name="description" content="Best PWA App in the world" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+        <meta name="msapplication-TileColor" content="#2B5797" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#000000" />
+
+        <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/icons/touch-icon-ipad.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/touch-icon-iphone-retina.png" />
+        <link rel="apple-touch-icon" sizes="167x167" href="/icons/touch-icon-ipad-retina.png" />
+
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        {/* <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" /> */}
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:url" content="https://yourdomain.com" />
+        <meta name="twitter:title" content="PWA App" />
+        <meta name="twitter:description" content="Best PWA App in the world" />
+        <meta
+          name="twitter:image"
+          content="https://yourdomain.com/icons/android-chrome-192x192.png"
+        />
+        <meta name="twitter:creator" content="@DavidWShadow" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="PWA App" />
+        <meta property="og:description" content="Best PWA App in the world" />
+        <meta property="og:site_name" content="PWA App" />
+        <meta property="og:url" content="https://yourdomain.com" />
+        <meta property="og:image" content="https://yourdomain.com/icons/apple-touch-icon.png" />
+
+        {/* <!-- apple splash screen images -->
+<!-- */}
+        <link
+          rel="apple-touch-startup-image"
+          href="/images/apple_splash_2048.png"
+          sizes="2048x2732"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/images/apple_splash_1668.png"
+          sizes="1668x2224"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/images/apple_splash_1536.png"
+          sizes="1536x2048"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/images/apple_splash_1125.png"
+          sizes="1125x2436"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/images/apple_splash_1242.png"
+          sizes="1242x2208"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/images/apple_splash_750.png"
+          sizes="750x1334"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/images/apple_splash_640.png"
+          sizes="640x1136"
+        />
+        {/* --> */}
       </Head>
 
       <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
